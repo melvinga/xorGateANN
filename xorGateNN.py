57c234e97f496c71761da9028d9290cedda44450
Melvin George <melvingeorge@gmail.com>
2020-12-31 01:17:18 +0530

    iteration is crashing. Will have to fix in next commit

--- xorGateNN.py
+++ xorGateNN.py
@@ -1,6 +1,9 @@
 import numpy as np
 from numpy import random
 
+lr = 0.5
+MSE_EXPECTED = 1 / (1 * 1000)
+
 # XOR gate inputs and corresponding output
 i0 = np.array([0, 0, 1, 1], np.float16)
 i1 = np.array([0, 1, 0, 1], np.float16)
@@ -34,9 +37,9 @@ for i in range(wh_rows):
     for j in range(wh_cols):
         wh[i,j] = random.rand() * 1 # 1 is upper value of 0/1 input set
 
-# Inputs for hidden layer after applying weights
-in_h = np.dot(wh, inputs)
-print("in_h: ",in_h)
+## Inputs for hidden layer after applying weights
+#in_h = np.dot(wh, inputs)
+#print("in_h: ",in_h)
 
 def sigmoid(x):
     val = 1 / (1 + np.exp(-x))
@@ -47,8 +50,8 @@ def derivativeOfSigmoid(x):
     val = val * (1 - val)
     return val
 
-out_h = np.vectorize(sigmoid)(in_h)
-print("out_h: ",out_h)
+#out_h = np.vectorize(sigmoid)(in_h)
+#print("out_h: ",out_h)
 
 # Weights for output layer
 #
@@ -60,6 +63,7 @@ wo_rows = len(wo)
 for i in range(wo_rows):
     wo[i] = random.rand() * 1 # 1 is upper value of 0/1 hidden set
 
+'''
 # Inputs for output layer after applying weights
 in_o = np.dot(wo, out_h)
 print("in_o: ",in_o)
@@ -77,10 +81,11 @@ print("mse_arr: ",mse_arr)
 
 MSE = np.sum(mse_arr)
 print("MSE: ",MSE)
+'''
 
 # Calculating partial derivatives for output layer
 def partialErrorOverOutputOfOutputNode(output, target):
-    val = np.subtract(output - target)
+    val = np.subtract(output, target)
     return val
 
 def partialOutputOverInputOfOutputNode(output):
@@ -91,9 +96,9 @@ def partialInputOverWeightOfOutputNode(outputOfHiddenLayer):
     val = outputOfHiddenLayer
     return val
 
-def partialErrorOverWeightAtOutputNode(mat1, mat2, mat3):
-    val = np.multiply(mat1, mat2)
-    val = np.multiply(mat3, np.transpose(val))
+def partialErrorOverWeightAtOutputNode(dErrOverOut, dOutOverIn, dInOverW):
+    val = np.multiply(dErrOverOut, dOutOverIn)
+    val = np.multiply(dInOverW, np.transpose(val))
     return val
 
 # Calculating partial derivatives for hidden layer
@@ -101,9 +106,11 @@ def partialInputOfOutputNodeOverOutputOfHiddenNode(weightsOfOutputLayer):
     val = weightsOfOutputLayer
     return val
 
-def partialErrorOverOutputOfHiddenNode(mat1, mat2, mat3):
-    val = np.multiply(mat1 * mat2)
-    val = (np.transpose(mat3)).dot(val)
+def partialErrorOverOutputOfHiddenNode(dErrOverOutOfOutputLayer, \
+                                        dOutOverInOfOutputLayer, \
+                                        dWeightedInputAtOutputNodeOverOutputOfHiddenLayer):
+    val = np.multiply(dErrOverOutOfOutputLayer * dOutOverInOfOutputLayer)
+    val = (np.transpose(dWeightedInputAtOutputNodeOverOutputOfHiddenLayer)).dot(val)
     return val
 
 def partialOutputOverInputOfHiddenNode(outputOfHiddenLayer):
@@ -114,22 +121,119 @@ def partialInputOverWeightOfHiddenNode(inputOfHiddenLayer):
     val = inputOfHiddenLayer
     return val
 
-def partialErrorOverWeightAtHiddenNode(mat1, mat2, mat3):
-    val = val = np.multiply(mat1, mat2)
-    val = np.multiply(mat3, np.transpose(val))
+def partialErrorOverWeightAtHiddenNode(dErrOverOut, dOutOverIn, dInOverW):
+    val = val = np.multiply(dErrOverOut, dOutOverIn)
+    val = np.multiply(dInOverW, np.transpose(val))
     return val
 
-# Calculate value of new weights
-def newValueOfWeights(lr, oldWeights, fr):
+# Calculate value of new weights for output layer
+def newValueOfWeightsForOutputLayer(lr, oldWeights, fr):
     val = -1
 
     if (oldWeights.shape == fr.shape):
         val = np.subtract(oldWeights, np.multiply(lr, fr))
+        val = np.transpose(val)
     else:
-        errMsg = "Order of matrices differ. Will not compute new weights."
+        errMsg = "Order of matrices differ."
+        errMsg += " "+"Will not compute new weights for output layer."
         print("ERROR: ",errMsg)
+        print("oldWeights: \n",oldWeights)
+        print("fr: \n",fr)
 
     return val
+
+# Calculate value of new weights for hidden layer
+def newValueOfWeightsForHiddenLayer(lr, oldWeights, fr):
+    val = -1
+
+    if (oldWeights.shape == fr.shape):
+        val = np.subtract(oldWeights, np.multiply(lr, fr))
+        val = val.reshape(1, 2)
+    else:
+        errMsg = "Order of matrices differ."
+        errMsg += " "+"Will not compute new weights for hidden layer."
+        print("ERROR: ",errMsg)
+        print("oldWeights: \n",oldWeights)
+        print("fr: \n",fr)
+
+    return val
+
+def oneIteration(inputs, o0, wo, wh, lr):
+    print("inputs: \n",inputs) # MEL_DEBUG
+    print("o0: \n",o0) # MEL_DEBUG
+    print("wo: \n",wo) # MEL_DEBUG
+    print("wh: \n",wh) # MEL_DEBUG
+    print("lr: \n",lr) # MEL_DEBUG
+    woNew = np.array([0, 0, 0], np.float16)
+    print("init: woNew: \n",woNew) # MEL_DEBUG
+    whNew = np.array([  [0, 0], \
+                        [0, 0], \
+                        [0, 0]], np.float16)
+    print("init: whNew: \n",whNew) # MEL_DEBUG
+    mse = 0.0
+    print("init: mse: \n",mse) # MEL_DEBUG
+    # Inputs for hidden layer after applying weights
+    in_h = np.dot(wh, inputs)
+    print("in_h: \n",in_h) # MEL_DEBUG
+    # Outputs of hidden layer
+    out_h = np.vectorize(sigmoid)(in_h)
+    print("out_h: \n",out_h) # MEL_DEBUG
+    # Inputs for output layer after applying weights
+    in_o = np.dot(wo, out_h)
+    print("in_o: \n",in_o) # MEL_DEBUG
+    # Outputs of output layer
+    out_o = np.vectorize(sigmoid)(in_o)
+    print("out_o: \n",out_o) # MEL_DEBUG
+    # Error in each output compared to what was expected
+    error = np.subtract(o0, out_o)
+    print("error: \n",error) # MEL_DEBUG
+    # Calculating mean squared error
+    mse_arr = 0.5 * np.square(error)
+    print("mse_arr: \n",mse_arr) # MEL_DEBUG
+    mse = np.sum(mse_arr)
+    print("mse: \n",mse) # MEL_DEBUG
+    # Calculate new weights of output layer
+    dErrorOverOutput_o = partialErrorOverOutputOfOutputNode(out_o, o0)
+    print("dErrorOverOutput_o: \n",dErrorOverOutput_o) # MEL_DEBUG
+    dOutputOverInput_o = partialOutputOverInputOfOutputNode(out_o)
+    print("dOutputOverInput_o: \n",dOutputOverInput_o) # MEL_DEBUG
+    dInputOverWeight_o = partialInputOverWeightOfOutputNode(in_o)
+    print("dInputOverWeight_o: \n",dInputOverWeight_o) # MEL_DEBUG
+    dErrorOverWeight_o = partialErrorOverWeightAtHiddenNode( \
+                                dErrorOverOutput_o, \
+                                dOutputOverInput_o, \
+                                dInputOverWeight_o)
+    print("dErrorOverWeight_o: \n",dErrorOverWeight_o) # MEL_DEBUG
+    woNew = newValueOfWeightsForOutputLayer(lr, wo, dErrorOverWeight_o)
+    print("woNew: \n",woNew) # MEL_DEBUG
+    # Calculate new weights of hidden layer
+    dWeightInAtOutputOverOutOfHidden = \
+        partialInputOfOutputNodeOverOutputOfHiddenNode(wo)
+    print("dWeightInAtOutputOverOutOfHidden: \n",\
+        dWeightInAtOutputOverOutOfHidden) # MEL_DEBUG
+    dErrorOverOutput_h = partialErrorOverOutputOfHiddenNode( \
+                            dErrorOverOutput_o, \
+                            dOutputOverInput_o, \
+                            dWeightInAtOutputOverOutOfHidden)
+    print("dErrorOverOutput_h: \n",dErrorOverOutput_h) # MEL_DEBUG
+    dOutputOverInput_h = partialOutputOverInputOfHiddenNode(out_h)
+    print("dOutputOverInput_h: \n",dOutputOverInput_h) # MEL_DEBUG
+    dInputOverWeight_h = partialInputOverWeightOfHiddenNode(inputs)
+    print("dInputOverWeight_h: \n",dInputOverWeight_h) # MEL_DEBUG
+    dErrorOverWeight_h = partialErrorOverWeightAtHiddenNode( \
+                            dErrorOverOutput_h, \
+                            dOutputOverInput_h, \
+                            dInputOverWeight_h)
+    print("dErrorOverWeight_h: \n",dErrorOverWeight_h) # MEL_DEBUG
+    whNew = newValueOfWeightsForHiddenLayer(lr, wh, dErrorOverWeight_h)
+    print("whNew: \n",whNew) # MEL_DEBUG
+    # Return mse of this iteration, and the new weights calculated
+    print("mse: \n",mse) # MEL_DEBUG
+    print("whNew: \n",whNew) # MEL_DEBUG
+    print("woNew: \n",woNew) # MEL_DEBUG
+    return mse, whNew, woNew
+
+mseAfter, whAfter, woAfter = oneIteration(inputs, o0, wo, wh, lr)
 
 '''
 # weights between i1 node and hidden layer nodes
